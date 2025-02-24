4a88dc8c16538dd2b9f84a9275cfe5eb19343b7c
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-02-24 12:50:01 +0300

    Added a Count Chart

--- app/(dashboard)/admin/page.jsx
+++ app/(dashboard)/admin/page.jsx
@@ -1,4 +1,5 @@
 import UserCard from "@/components/UserCard";
+import CountChart from "@/components/CountChart";
 
 export default function AdminPage(){
 
@@ -8,12 +9,26 @@ export default function AdminPage(){
 
 
             {/*    Left*/}
-            <div className="w-full lg:w-2/3">
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
+                {/*User Cards*/}
                 <div className="flex gap-4 justify-between flex-wrap">
                     <UserCard/>
                     <UserCard/>
                     <UserCard/>
                     <UserCard/>
+                </div>
+                {/*Middle Charts*/}
+                <div className=" flex flex-col gap-4 lg:flex-row">
+                    {/*Count Chart*/}
+                    <div className="w-full h-[450px] lg:w-1/3">
+                        <CountChart />
+                    </div>
+
+                {/*    Attendance Chart*/}
+                    <div>
+
+                    </div>
+
                 </div>
 
             </div>
---
+++ components/CountChart.jsx
@@ -0,0 +1,83 @@
+"use client"
+
+import { RadialBarChart, RadialBar, Legend, ResponsiveContainer } from 'recharts';
+import Image from "next/image";
+
+const data = [
+
+
+    {
+        name: 'Total',
+        count: 106,
+        pv: 2400,
+        fill: 'white',
+    },
+
+    {
+        name: 'Girls',
+        count: 53,
+        pv: 2400,
+        fill: '#FAE27C',
+    },
+    {
+        name: 'Boys',
+        count: 53,
+        pv: 4567,
+        fill: '#C3EBFA  ',
+    }
+];
+
+const style = {
+    top: '50%',
+    right: 0,
+    transform: 'translate(0, -50%)',
+    lineHeight: '24px',
+};
+
+export default function CountChart() {
+    const demoUrl = 'https://codesandbox.io/p/sandbox/simple-radial-bar-chart-gnwjjg';
+
+        return (
+            <div className="bg-white  w-full h-full p-4 rounded-xl">
+                {/*  Title  */}
+                <div className="flex justify-between items-center">
+                    <h1>Students</h1>
+                    <Image src="/moreDark.png" alt="" width={20} height={20}/>
+                </div>
+
+                {/*    Chart*/}
+                <div className="w-full h-[75%] relative">
+                    <ResponsiveContainer >
+                        <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={32} data={data}>
+                            <RadialBar
+                                minAngle={15}
+                                // label={{ position: 'insideStart', fill: '#fff' }}
+                                background
+                                clockWise
+                                dataKey="count"
+                            />
+                            {/*<Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={style} />*/}
+                        </RadialBarChart>
+                    </ResponsiveContainer>
+                    <Image src="/maleFemale.png" alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
+                </div>
+
+            {/*    Bottom*/}
+                <div className="flex justify-center gap-16">
+                    <div className="flex flex-col gap-1 items-start">
+                        <div className="w-5 h-5 bg-lamaSky rounded-full"></div>
+                        <h1 className="font-bold">1,234</h1>
+                        <h2 className="font-xs text-gray-300">Boys (55%)</h2>
+                    </div>
+                    <div className="flex flex-col gap-1 items-start">
+                        <div className="w-5 h-5 bg-lamaYellow rounded-full"></div>
+                        <h1 className="font-bold">1,234</h1>
+                        <h2 className="font-xs text-gray-300">Girls (45%)</h2>
+                    </div>
+                </div>
+
+
+            </div>
+        );
+
+}
