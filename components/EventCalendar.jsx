6af0a839f6566affa23a2c632759b4f9acbf7a97
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-02-26 11:03:42 +0300

    Added a Events to the Calendar

--- components/EventCalendar.jsx
+++ components/EventCalendar.jsx
@@ -3,6 +3,7 @@
 import {useState} from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
+import Image from "next/image";
 
 
 export default function EventCalendar() {
@@ -30,8 +31,30 @@ export default function EventCalendar() {
     ];
 
     return (
-        <div>
-            <Calendar  onChange={setValue} value={value} className="bg-white p-4 rounded-md"/>
+        <div className="bg-white p-4 rounded-md">
+            <Calendar onChange={setValue} value={value}/>
+            <div>
+                <div className="flex justify-between items-center my-4">
+                    <h1 className="font-bold">Events</h1>
+                    <Image src="/moreDark.png" alt="" width={20} height={20}/>
+                </div>
+                <div className="flex flex-col gap-4 ">
+                {events.map((event) => (
+                    <div key={event.id} className="p-4 border-2 border-gray-100 rounded-md">
+
+                        <div className="flex justify-between items-center">
+                            <h1 className="font-semibold text-gray-600">{event.title}</h1>
+                            <span className="text-gray-300 text-xs">{event.time}</span>
+                        </div>
+
+                        <p className="mt-2 text-sm">{event.description}</p>
+
+                    </div>
+                ))}
+            </div>
+            </div>
         </div>
+
+
     )
 }
