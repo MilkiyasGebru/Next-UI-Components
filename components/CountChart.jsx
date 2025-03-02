0f312642ef835b42ef84d0a4c3d24e5b7f657892
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-02 20:42:31 +0300

    added comments for more description

--- components/CountChart.jsx
+++ components/CountChart.jsx
@@ -39,13 +39,13 @@ export default function CountChart() {
 
         return (
             <div className="bg-white  w-full h-full p-4 rounded-xl">
-                {/*  Title  */}
+                {/*  Title Part */}
                 <div className="flex justify-between items-center">
                     <h1 className="text-lg font-bold text-black">Students</h1>
                     <Image src="/moreDark.png" alt="" width={20} height={20}/>
                 </div>
 
-                {/*    Chart*/}
+                {/*    Chart Part*/}
                 <div className="w-full h-[75%] relative">
                     <ResponsiveContainer >
                         <RadialBarChart cx="50%" cy="50%" innerRadius="40%" outerRadius="100%" barSize={32} data={data}>
@@ -62,7 +62,7 @@ export default function CountChart() {
                     <Image src="/maleFemale.png" alt="" width={50} height={50} className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
                 </div>
 
-            {/*    Bottom*/}
+            {/*    Bottom Part*/}
                 <div className="flex justify-center gap-16">
                     <div className="flex flex-col gap-1 items-start">
                         <div className="w-5 h-5 bg-lamaSky rounded-full"></div>
