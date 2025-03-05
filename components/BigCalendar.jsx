eaff545af1ae3519199d8f3f109a2964a5c0fc02
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-03-05 10:44:23 +0300

    Added more styles to the calendar

--- components/BigCalendar.jsx
+++ components/BigCalendar.jsx
@@ -1,12 +1,13 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import {Calendar, momentLocalizer, Views} from 'react-big-calendar'
 import moment from 'moment'
 import "react-big-calendar/lib/css/react-big-calendar.css"
+import {useState} from "react";
 
 const localizer = momentLocalizer(moment)
 //  Found a bug on the Big calendar
 export default function BigCalendar() {
 
-     const calendarEvents = [
+    const calendarEvents = [
         {
             title: "Math",
             allDay: false,
@@ -150,15 +151,26 @@ export default function BigCalendar() {
             end: new Date(2024, 7, 16, 14, 45),
         },
     ];
+    const [view,setView] = useState(Views.WORK_WEEK)
+
+    const handleOnChangeView = (selected_view) => {
+        console.log(selected_view," the selected_view")
+        setView(selected_view)
+    }
 
     return (
-        <div>
+        <div className="h-[55%]">
             <Calendar
                 localizer={localizer}
                 events={calendarEvents}
                 startAccessor="start"
                 endAccessor="end"
-                style={{ height: 500 }}
+                views={["work_week","day"]}
+                view={view}
+                style={{ height: "95%" }}
+                onView={handleOnChangeView}
+                min={new Date(2025,2,5,8)}
+                max={new Date(2025,2,5,17)}
             />
         </div>
     )
